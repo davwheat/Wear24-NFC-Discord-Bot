b83ee941101b0e19f67953263de69dd0f4e6c5f9
David Wheatley <blokmaster.help@gmail.com>
2019-02-07 18:15:57 +0000

    Add project files.

---
+++ Wear24-NFC-Discord-Bot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Wear24_NFC_Discord_Bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
